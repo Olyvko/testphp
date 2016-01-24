114f14c7d3121519fca6d7194b8f5f46378b27c3
promen717 <oluvko@mail.ru>
2016-01-24 21:23:20 +0200

    tttt

--- orderexport/models/ecaddressbook.php
+++ orderexport/models/ecaddressbook.php
@@ -35,7 +35,7 @@ class ecaddressbook extends ecommerce {
     public function nottmy2(){
         return 123;
     }
-    public function nottmy7(){
+    public function nottmy89(){
         return 123;
     }
 
